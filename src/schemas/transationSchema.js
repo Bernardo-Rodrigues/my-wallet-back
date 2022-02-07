41ba4c3be39b9f3aceeda0b28aec50b282ee4f33
Bernardo Rodrigues <bernardo.rodrigues06@unifei.edu.br>
2022-02-06 22:10:42 -0300

    chore: increase transaction name max size

--- src/schemas/transationSchema.js
+++ src/schemas/transationSchema.js
@@ -2,7 +2,7 @@ import joi from "joi"
 
 export const transactionSchema = joi.object({
     value: joi.number().max(999999).required(),
-    desc: joi.string().max(16).required(),
+    desc: joi.string().max(25).required(),
     type: joi.string().valid("entry", "output").required()
 });
 
